f6de08aa925e843684fc5439e4cde01c1c7f632b
Emily Marin <emily.s.marin@icloud.com>
2023-02-16 23:30:39 -1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import OFSVG from "./svgs/OFSVG";
+import TwitterSVG from "./svgs/TwitterSVG";
+import InstaSVG from "./svgs/InstaSVG";
+import "./App.css";
+import SnapSVG from "./svgs/SnapSVG";
+import TikTokSVG from "./svgs/TikTokSVG";
+import CashappSVG from "./svgs/CashappSVG";
+import VenmoSVG from "./svgs/VenmoSVG";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-purple-300 min-h-screen flex flex-col items-center">
+      <h1 className="text-6xl font-display text-purple-700 text-center  ">
+        Millie Melons
+      </h1>
+      <img src="Profile.png" className="max-w-xs h-auto rounded-full" />
+      <div className="flex flex-col">
+        <div className="shadow hover:bg-purple-400 m-2 w-max bg-purple-500 text-white rounded-md p-2 ">
+          <a
+            className="grid grid-cols-[minmax(0px,36px),minmax(0,1fr)] items-center gap-3"
+            href="https://onlyfans.com/millie_melons"
+          >
+            <OFSVG />
+            Free OnlyFans
+          </a>
+        </div>
+        <div className="shadow hover:bg-purple-400  bg-purple-500 text-white rounded-md p-2 m-2">
+          <a
+            className="grid grid-cols-[minmax(0px,36px),minmax(0,1fr)] items-center gap-3"
+            href="https://twitter.com/millie_melons"
+          >
+            <TwitterSVG />
+            Twitter
+          </a>
+        </div>
+        <div className="shadow hover:bg-purple-400  bg-purple-500 text-white rounded-md p-2 m-2">
+          <a
+            className="grid grid-cols-[minmax(0px,36px),minmax(0,1fr)] items-center gap-3"
+            href="https://www.instagram.com/millie_melons/"
+          >
+            <InstaSVG />
+            Instagram
+          </a>
+        </div>
+        <div className="shadow hover:bg-purple-400  bg-purple-500 text-white rounded-md p-2 m-2">
+          <a
+            className="grid grid-cols-[minmax(0px,36px),minmax(0,1fr)] items-center gap-3"
+            href="https://www.snapchat.com/add/millie_melons"
+          >
+            <SnapSVG />
+            Snapchat
+          </a>
+        </div>
+        <div className="shadow hover:bg-purple-400  bg-purple-500 text-white rounded-md p-2 m-2">
+          <a
+            className="grid grid-cols-[minmax(0px,36px),minmax(0,1fr)] items-center gap-3"
+            href="https://www.tiktok.com/@milliemelonsxoxo?lang=en"
+          >
+            <TikTokSVG />
+            TikTok
+          </a>
+        </div>
+        <div className="shadow hover:bg-purple-400  bg-purple-500 text-white rounded-md p-2 m-2">
+          <a
+            className="grid grid-cols-[minmax(0px,36px),minmax(0,1fr)] items-center gap-3"
+            href="http://cash.app/$milliemelonsxxx"
+          >
+            <CashappSVG />
+            CashApp
+          </a>
+        </div>
+        <div className="shadow hover:bg-purple-400  bg-purple-500 text-white rounded-md p-2 m-2">
+          <a
+            className="grid grid-cols-[minmax(0px,36px),minmax(0,1fr)] items-center gap-3"
+            href="https://venmo.com/u/millie_melons"
+          >
+            <VenmoSVG />
+            Venmo
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
